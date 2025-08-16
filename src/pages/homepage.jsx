de2341e301b572a72c19def449e8874a75101217
Anush Shrestha <anush.stha980@outlook.com>
2025-08-17 00:12:38 +0545

    Add: basic todo input

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './homepage.css'
 
 function DailyDiary() {
+    const [diary, setDiary] = useState([]);
+    const [temp, setTemp] = useState("");
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (!temp.trim()) return;
+        setDiary([...diary, temp]);
+        setTemp("");
+    }
+
     return (
         <>
+            <div className="main-container">
+                <div className="left-container">
+                    <div className="task-container">
+                        <div className="left-task-container">
+                            <span>Tasks Done</span>
+                            <p>Lets get it done!</p>
+                            <div className="progress-container">
+                            <progress value="50" max="100" className='progress-bar'/>
+                        </div>
+                        </div>
+                        <div className="right-task-container">
+                            <div className="circle-container">
+                                <span>1/3</span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <form onSubmit={submitHandler} className='input-form'>
+                        <input type='text' value={temp} onChange={(e) => setTemp(e.target.value)} placeholder='Whats your goal for today...' className='todo-add' />
+                        <button type='sumbit' className='add-button'><span>+</span></button>
+                    </form>
+                    <ol>
+                        {diary.map((diaries, index) => {
+                            return <li key={index}>{diaries}</li>
+                        })}
+                    </ol>
+
+                </div>
+                <div className="right-container">
+                </div>
+            </div>
         </>
     )
 }
